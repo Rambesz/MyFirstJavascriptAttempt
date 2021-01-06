54cde81bb3c4fa5363f6ed7be1df224b03e10932
Attila Paucsa <rambihno88@gmail.com>
2021-01-06 07:56:31 +0100

    first day

---
+++ home.js
@@ -0,0 +1,11 @@
+console.log('Hello');
+//alert('yoo');
+
+var b = 'smoothie';
+console.log(b);
+
+//Modify the connected HTML docs someText named id tag
+//document.getElementById('someText').innerHTML = 'Hey there';
+
+var age = prompt('How old are you?');
+document.getElementById('someText').innerHTML = age;
